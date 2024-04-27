76a7d70aa571769b8564a908972f71eac11da366
Michael Andres Martinez Guzman <mic.martinez@udla.edu.co>
2024-04-26 22:39:37 -0500

    sí

--- HDUA/Controllers/AdminController.cs
+++ HDUA/Controllers/AdminController.cs
@@ -13,5 +13,10 @@ namespace HDUA.Controllers
         {
             return View();
         }
+
+        public IActionResult GestionUsuario()
+        {
+            return View();
+        }
     }
 }
